212da2e3c2fc08830b249c3097c63e30625e69b8
Jane Wen <JaneWenLavender@outlook.com>
2023-02-10 09:59:28 +0100

    Apply HashRoute

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../common/header/Header";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
